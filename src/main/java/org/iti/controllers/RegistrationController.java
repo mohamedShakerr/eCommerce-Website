e5ad08d43aaa7754241cfde4cc9e983f7d15c511
MostafaYahia <mostafayehyax23@gmail.com>
2021-03-22 22:52:21 +0200

    Add register controller and modify mapping for the page

---
+++ src/main/java/org/iti/controllers/RegistrationController.java
@@ -0,0 +1,23 @@
+package org.iti.controllers;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class RegistrationController extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+
+        // 
+    }
+}
